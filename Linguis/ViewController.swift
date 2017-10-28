4dcc9c5be6c2eaef57bf2b15e771602c22910a96
Yang Liu <byliuyang11@gmail.com>
2017-10-28 15:37:59 -0400

    Initial Commit

---
+++ Linguis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Linguis
+//
+//  Created by Yang Liu on 10/28/17.
+//  Copyright © 2017 Harry Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
